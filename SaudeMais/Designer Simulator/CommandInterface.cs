ade8ccb9a106e262dff30bf9b0b9b8e826193d80
Anderson Rafhael <rafha.barbosa98@gmail.com>
2020-02-10 12:38:26 -0300

    Delete CommandInterface.cs

--- SaudeMais/Designer Simulator/CommandInterface.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-//This is a basic interface with a single required
-//method.
-public interface CommandInterface {
-    void Execute(GameObject canvasName, bool action);
-}
